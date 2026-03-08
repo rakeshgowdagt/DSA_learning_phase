2d142fe793203ebf94269e42f9a0e6a3e8024ff4
Rakesh Gowda <rakeshgowdagt@gmail.com>
2026-03-08 23:37:05 +0530

    Update square_of_sorted_array.java

--- Arrays/square_of_sorted_array.java
+++ Arrays/square_of_sorted_array.java
@@ -7,3 +7,24 @@ class Solution {
   return nums;
  }
 }
+
+/*below solution:with O(n) time complexity*/
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0;
+        int right = n - 1;
+        int k = n - 1;
+        while(left <= right){
+            if(Math.abs(nums[left]) > Math.abs(nums[right])){
+                result[k] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[k] = nums[right] * nums[right];
+                right--;
+            }
+            k--;
+        }
+        return result;
+    }}
